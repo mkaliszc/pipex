9fee8a261de5f441c8c00b737eee70094d1f2ab9
Milan Kaliszczak <mkaliszc@c2r3p1.42lehavre.fr>
2025-01-08 00:29:14 +0100

    project finished

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mkaliszc <mkaliszc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/21 19:11:06 by mkaliszc          #+#    #+#             */
-/*   Updated: 2025/01/08 00:28:26 by mkaliszc         ###   ########.fr       */
+/*   Updated: 2025/01/08 00:29:10 by mkaliszc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv, char **envp)
 	t_data	*data;
 
 	if (argc < 5)
-		return(ft_putstr_fd("too few arguments", 2), 0);
+		return (ft_putstr_fd("too few arguments", 2), 0);
 	data = malloc(sizeof(t_data));
 	if (data == NULL)
 		return (perror("allocation data struct failed"), 1);
